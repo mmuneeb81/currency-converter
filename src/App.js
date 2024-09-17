0c5cd3f0d90b0caa8e71e8bfe431fb0a2632391f
Mohammad Muneeb <mmuneeb81@icloud.com>
2024-09-16 21:10:52 -0500

    Tailwind config

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Currency Converter v1</h2>
+      <h2 class='ml-8'>Currency Converter v1</h2>
     </div>
   );
 }
